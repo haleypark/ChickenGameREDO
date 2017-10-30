8f4fa66167eedd184365eb675b590b28398c786f
Unknown <10706843@my.uvu.edu>
2017-10-30 17:38:49 -0600

    chicken redo

---
+++ ChickenGame2/Assets/Scripts/ChickenFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenFollow : MonoBehaviour {
+
+	// public Transform player;
+
+	// public Transform chicken;
+
+	// void Start () {
+
+	// 	player = GameObject.FindWithTag("Player").transform;
+		
+	// }
+	
+
+	// void Update () {
+	// 	transform.LookAt (player);
+	// 	transform.Translate (Vector3.forward * Time.deltaTime);	
+	// }
+
+
+		//destroy(gameObject);
+		//scoremanager.AddPoints(points);
+	// }
+
+
+}
+
---
+++ ChickenGame2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float moveSpeed;
+
+	public float turnSpeed;
+
+	public float jumpHeight;
+
+	public float strafeSpeed;
+
+	void Start () {
+		
+	}
+	
+	// Time.deltaTime
+	void Update () {
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+		var x = Input.GetAxis("Strafe")* Time.deltaTime * strafeSpeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+		transform.Translate(x,0,0);
+	}
+}
